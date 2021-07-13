767c108ab955230947d4f9d47fc4cb30cf1aeca6
TomPretty <tompretty2@gmail.com>
2021-07-13 22:43:49 +0100

    use sorted set for tests

--- src/views.js
+++ src/views.js
@@ -11,33 +11,45 @@ function viewLogger({ windowTimeMs }) {
 
     await client
       .multi()
-      .sadd(TESTS_KEY, test)
+      .zadd(TESTS_KEY, now, test)
       .zadd(TESTS_VIEWS_KEY + test, now, now)
       .expire(TESTS_VIEWS_KEY + test, windowTimeMs / 1000)
       .exec();
   }
 
   async function getViews(test) {
-    const now = Date.now();
-    const expirationMs = now - windowTimeMs;
-
     const [, [, reply]] = await client
       .multi()
-      .zremrangebyscore(TESTS_VIEWS_KEY + test, 0, expirationMs)
+      .zremrangebyscore(TESTS_VIEWS_KEY + test, 0, expirationMs())
       .zcard(TESTS_VIEWS_KEY + test)
       .exec();
 
     return parseInt(reply);
   }
 
+  async function getAllTests() {
+    const [, [, tests]] = await client
+      .multi()
+      .zremrangebyscore(TESTS_KEY, 0, expirationMs())
+      .zrange(TESTS_KEY, 0, -1)
+      .exec();
+
+    return tests;
+  }
+
   async function getAllViews() {
-    const tests = await client.smembers(TESTS_KEY)
+    const tests = await getAllTests();
 
     const views = {};
     for (let test of tests) {
       views[test] = await getViews(test);
     }
     return views;
+  }
+
+  // --- utils --- //
+  function expirationMs() {
+    return Date.now() - windowTimeMs;
   }
 
   return { getViews, getAllViews, logView }
